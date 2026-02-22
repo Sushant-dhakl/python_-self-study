6e5378932b63f285341109481c0606978ef7577d
Sushant Dhakal -----(motas) <sushantdhakal98@gmail.com>
2026-02-22 14:26:23 +0545

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,79 @@
+print("hello world!")
+print("hello world.","I am here !")
+
+# print fuction will print anything the user want 
+# always put Single "" or '' inside the bracket to print statement 
+# print("hello world.","I am here !") coma can be used to print another statement but it will give output in the same line
+ 
+ # VARIABLES
+
+Name = "sushant" # strings should be writte in double cotes" 
+age = 20  # no need for cotes for integer 
+time = 15.99 # no need for cotes in floting numbers 
+live = True # it is boolen 
+
+print(Name,age,time)
+print("my name is :",Name )
+
+# rules of identifiers 
+# variable1,Variable_1,variable_a_b are valid 
+# 1Variable is not valid, no num should be present before variable name
+# no special character like @,#,$,% can be used in variables
+
+# arithametic operation
+
+a = 1
+b = 35 
+sum = a+b
+print(sum)
+ 
+# operator in python 
+
+#(+,-,*,%,/,**) arithmetic operator 
+# relational operators (==,!=,>,<,<=,>=)
+#assignment operator (=,+=,-=,%=,**=)
+# logical operators( not,and ,or)
+
+print (a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # gives reminder 
+print(a**b) #a^b
+
+num = 10
+num += 10 # num = num+10
+print("num :",num )
+
+# logical operation 
+a = 50
+b = 30
+print(a>b) # ans will be true 
+print(not(a>b)) # ans willbe false , not will oppose the answer
+ 
+
+val_1 = 15
+val_2 =12
+
+print(" and operator;", val_1>val_2 and a>b) # answer will be true
+print(" and operator;", val_1<val_2 and a>b) # answer will be false
+
+# and operator will only give answer true when both of the statement are correct if not answer will be false . 
+
+print(" or operator;", val_1>val_2 or a>b) # answer will be true
+print(" or operator;", val_1<val_2 or a>b) # answer will be true
+print(" or operator;", val_1<val_2 or a<b) # answer will be false
+
+
+# or operator will only give answer false when both of the statement are false if any statement is true the answer will be true . 
+
+
+
+
+
+
+
+
+
+
+
